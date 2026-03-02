01f31c3f2381a50c4c000c19cc9a0f289af82113
tata1009 <tender_passion@icloud.com>
2026-03-02 12:22:06 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let nav = document.querySelector("#navarea");
+let btn = document.querySelector(".toggle-btn");
+let mask = document.querySelector("#mask");
+
+btn.onclick = () => {
+  nav.classList.toggle("open");
+};
+
+mask.onclick = () => {
+  nav.classList.toggle("open");
+};
