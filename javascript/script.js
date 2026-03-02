27b4ade70caa62e46658667ffde72f7f11773b32
tata1009 <tender_passion@icloud.com>
2026-03-02 12:33:22 +0900

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+let nav = document.querySelector("#navarea");
+let btn = document.querySelector(".toggle-btn");
+let mask = document.querySelector("#mask");
+
+btn.onclick = () => {
+  nav.classList.toggle("open");
+};
+
+mask.onclick = () => {
+  nav.classList.toggle("open");
+};
+
